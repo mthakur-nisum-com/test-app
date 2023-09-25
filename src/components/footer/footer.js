d69cb6dfe305b560d61ced6b6646473fc6318ee0
manoj thakur <manoj.thakur1237@gmail.com>
2023-09-26 02:33:03 +0530

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,6 +3,6 @@ import './styles.scss';
 
 export const Footer = () => (
     <footer>
-        <a href='https://github.com/mthakur-nisum-com/my-app' target='_blank'>Code</a>
+        <a href='https://github.com/mthakur-nisum-com/test-app' target='_blank'>Code</a>
     </footer>
 )
